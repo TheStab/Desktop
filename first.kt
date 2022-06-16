b549865a584d9df9d9efd98abb4af01b3b3c679b
TheStab <74876811+TheStab@users.noreply.github.com>
2022-06-17 01:05:39 +0400

    Update first.kt

--- first.kt
+++ first.kt
@@ -2,6 +2,7 @@ fun main() {
 
     while(true) {
         
+        //Initialization of variables
         println("შეიყვანეთ X ცვლადის მნიშვნელობა")
         val x = readLine()
         println("შეიყვანეთ Y ცვლადის მნიშვნელობა")
@@ -9,11 +10,12 @@ fun main() {
         var z = 0.0
         var ANSWER :String
         
+        //Check Inputs
         try {
             if (x != null && y != null)
                 z = x.toDouble() + y.toDouble()
             println("X და Y ჯამი არის: $z ")
-            
+        //Check Loop
             while (true) {
                 println("გსურთ პროგრამის ხელახლა დაწყება <Y/N>?")
                 ANSWER = readLine().toString()
@@ -24,6 +26,7 @@ fun main() {
                 break
             else continue
         }
+        //Exception
         catch (exception: NumberFormatException) {
             println("მონაცემები არასწორია, სცადეთ ხელახლა")
         }
